69cdb64d4ea42106bb1e8a4f12967edc456d7bc5
Jorge Alcaras <jorge.alcaras@cceo.com.mx>
2024-02-15 10:20:17 -0600

    Feature (Counter): :sparkles: The code has been optimized and home screen has been removed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-//import 'package:counter_app/screens/home_screen.dart';
 import 'package:counter_app/screens/counter_screen.dart';
 
 void main() {
@@ -11,7 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      //home: HomeScreen()
       home: CounterScreen(),
     );
   }
--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -7,59 +7,83 @@ class CounterScreen extends StatefulWidget {
 }
 
 class _CounterScreenState extends State<CounterScreen> {
-  int i = 0;
+  int counter = 0;
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  void refresh() {
+    counter = 0;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text('Counter', style: TextStyle(color: Colors.white)),
+          elevation: 10,
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.white,
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('Clicks',
+                style: TextStyle(color: Colors.black, fontSize: 30)),
+            Text('$counter',
+                style: const TextStyle(color: Colors.blue, fontSize: 50)),
+          ],
+        )),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: CustomButton(
+            decreaseCounter: decrease,
+            refreshCounter: refresh,
+            incressCounter: increase));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  int counter = 0;
+  final Function incressCounter;
+  final Function refreshCounter;
+  final Function decreaseCounter;
+
+  CustomButton(
+      {Key? key,
+      required this.decreaseCounter,
+      required this.refreshCounter,
+      required this.incressCounter})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+      FloatingActionButton(
         backgroundColor: Colors.green,
-        title: const Text('Counter', style: TextStyle(color: Colors.white)),
-        elevation: 10,
-        centerTitle: true,
+        elevation: 30,
+        onPressed: () => decreaseCounter(),
+        child: const Icon(Icons.remove),
       ),
-      backgroundColor: Colors.white,
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text('Clicks',
-              style: TextStyle(color: Colors.black, fontSize: 30)),
-          Text('$i', style: const TextStyle(color: Colors.blue, fontSize: 50)),
-        ],
-      )),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton:
-          Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-        FloatingActionButton(
+      FloatingActionButton(
           backgroundColor: Colors.green,
           elevation: 30,
-          onPressed: () {
-            //print('Clicked ${i++} times');
-            i--;
-            setState(() {});
-          },
-          child: const Icon(Icons.remove),
-        ),
-        FloatingActionButton(
-            backgroundColor: Colors.green,
-            elevation: 30,
-            onPressed: () {
-              //print('Clicked ${i++} times');
-              i = 0;
-              setState(() {});
-            },
-            child: const Icon(Icons.refresh)),
-        FloatingActionButton(
-          backgroundColor: Colors.green,
-          elevation: 30,
-          onPressed: () {
-            //print('Clicked ${i++} times');
-            i++;
-            setState(() {});
-          },
-          child: const Icon(Icons.add),
-        ),
-      ]),
-    );
+          onPressed: () => refreshCounter(),
+          child: const Icon(Icons.refresh)),
+      FloatingActionButton(
+        backgroundColor: Colors.green,
+        elevation: 30,
+        onPressed: () => incressCounter(),
+        child: const Icon(Icons.add),
+      ),
+    ]);
   }
 }
